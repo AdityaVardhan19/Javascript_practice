bea694534aa54d65e49ccb3562aadac2f9963c1f
AdityaVardhan19 <adityavardhan236@gmail.com>
2026-06-30 00:02:33 +0530

    Built a greetingcard and prfile page

---
+++ introduction/app.js
@@ -0,0 +1,45 @@
+/*let changeText= document.getElementById("title")
+let btn= document.getElementById("btn")
+
+function changeHeading() {
+    changeText.textContent = "JavaScript is the dynamic language"
+    changeText.style.color ="blue"
+    changeText.style.backgroundColor ="red"
+
+}*/
+
+/*let heading = document.getElementById("title")
+let message = document.getElementById("message")
+let button = document.getElementById("btn")
+
+function showMessage(){
+
+    heading.textContent = "Hello Students!"
+
+    heading.style.color = "red"
+
+    message.textContent = "Happy Birthday! May this special day bring you love, peace, and countless blessings."
+
+    message.style.color = "purple"
+
+    message.style.backgroundColor = "lavender"
+
+}*/
+
+let heading = document.getElementById("title")
+let info = document.getElementById("name")
+let button = document.getElementById("btn")
+
+function showInfo(){
+
+    heading.textContent = "Hi, I'm Aditya Vardhan!"
+
+    heading.style.color = "white"
+    heading.style.backgroundColor = "crimson"
+
+    info.textContent = "Currently I'm learning JavaScript and Full Stack Development to become a Software Engineer."
+
+    info.style.color = "darkblue"
+    info.style.backgroundColor = "gold"
+
+}
